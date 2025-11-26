bf38ba4cf69bb9903f25def6d2da8cb0c0ac7b27
Max Onyx <devops@maxim.run>
2025-11-25 18:03:11 -0800

    feat: generate index.html for each repo

---
+++ imports.tf
@@ -0,0 +1,120 @@
+import {
+  to = module.repos["repo-as-a-code"].github_repository.this
+  id = "repo-as-a-code"
+}
+import {
+  to = module.repos["elk_services"].github_repository.this
+  id = "elk_services"
+}
+import {
+  to = module.repos["magzim21"].github_repository.this
+  id = "magzim21"
+}
+import {
+  to = module.repos["obsidian-notes"].github_repository.this
+  id = "obsidian-notes"
+}
+import {
+  to = module.repos["quick_ops"].github_repository.this
+  id = "quick_ops"
+}
+import {
+  to = module.repos["quick-ops"].github_repository.this
+  id = "quick-ops"
+}
+import {
+  to = module.repos["job_parser"].github_repository.this
+  id = "job_parser"
+}
+import {
+  to = module.repos["Kanban-Board"].github_repository.this
+  id = "Kanban-Board"
+}
+import {
+  to = module.repos["terragrunt-sync"].github_repository.this
+  id = "terragrunt-sync"
+}
+import {
+  to = module.repos["ship-fast-ts"].github_repository.this
+  id = "ship-fast-ts"
+}
+import {
+  to = module.repos["english-new"].github_repository.this
+  id = "english-new"
+}
+import {
+  to = module.repos["template"].github_repository.this
+  id = "template"
+}
+import {
+  to = module.repos["crypto-seed-phrase-generator"].github_repository.this
+  id = "crypto-seed-phrase-generator"
+}
+import {
+  to = module.repos["dotfiles"].github_repository.this
+  id = "dotfiles"
+}
+import {
+  to = module.repos["safedial"].github_repository.this
+  id = "safedial"
+}
+import {
+  to = module.repos["elevate-sec"].github_repository.this
+  id = "elevate-sec"
+}
+import {
+  to = module.repos["magz8s"].github_repository.this
+  id = "magz8s"
+}
+import {
+  to = module.repos["kubespray-lite"].github_repository.this
+  id = "kubespray-lite"
+}
+import {
+  to = module.repos["magzim21.github.io"].github_repository.this
+  id = "magzim21.github.io"
+}
+import {
+  to = module.repos["aws_terraform"].github_repository.this
+  id = "aws_terraform"
+}
+import {
+  to = module.repos["Ansible-gather-information"].github_repository.this
+  id = "Ansible-gather-information"
+}
+import {
+  to = module.repos["ansible_change_hostnames"].github_repository.this
+  id = "ansible_change_hostnames"
+}
+import {
+  to = module.repos["zabbix_agents_ansible"].github_repository.this
+  id = "zabbix_agents_ansible"
+}
+import {
+  to = module.repos["docker-zabbix-mysql"].github_repository.this
+  id = "docker-zabbix-mysql"
+}
+import {
+  to = module.repos["OOP-csv-editor"].github_repository.this
+  id = "OOP-csv-editor"
+}
+import {
+  to = module.repos["Django-Flowers-Shop"].github_repository.this
+  id = "Django-Flowers-Shop"
+}
+import {
+  to = module.repos["PHP-MySQL-JS_Payroll_System"].github_repository.this
+  id = "PHP-MySQL-JS_Payroll_System"
+}
+import {
+  to = module.repos["Rolling-dice-game"].github_repository.this
+  id = "Rolling-dice-game"
+}
+import {
+  to = module.repos["Parking-app"].github_repository.this
+  id = "Parking-app"
+}
+import {
+  to = module.repos["Registration-form"].github_repository.this
+  id = "Registration-form"
+}
