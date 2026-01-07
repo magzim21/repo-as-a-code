2c1e3881f5cccaccf8f1620c28bf8e52e2b8e3ca
Max Onyx <devops@maxim.run>
2026-01-06 20:57:42 -0800

    feat: s3 backend resources

---
+++ backend.tf
@@ -0,0 +1,29 @@
+data "aws_caller_identity" "current" {}
+
+
+module "terraform_state_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.9.1"
+
+  bucket        = "terraform-state-${data.aws_caller_identity.current.account_id}-${basename(abspath(path.root))}"
+  force_destroy = false
+
+  versioning = {
+    status = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+}
+
