cb52a02051b786f7f74762bf919918b21cc6bfc1
Max Onyx <devops@maxim.run>
2026-04-20 14:45:36 -0700

    feat: datadog gha variables

--- datadog.tf
+++ datadog.tf
@@ -35,18 +35,18 @@ resource "datadog_synthetics_test" "test_uptime" {
 }
 
 resource "github_actions_secret" "datadog_api_key" {
-  repository       = "repo-as-a-code"
-  secret_name      = "DD_API_KEY"
-  plaintext_value  = "placeholder-added-manually"
+  repository      = "repo-as-a-code"
+  secret_name     = "DD_API_KEY"
+  plaintext_value = "placeholder-added-manually"
   lifecycle {
     ignore_changes = [remote_updated_at]
   }
 }
 
 resource "github_actions_secret" "datadog_app_key" {
-  repository       = "repo-as-a-code"
-  secret_name      = "DD_APP_KEY"
-  plaintext_value  = "placeholder-added-manually"
+  repository      = "repo-as-a-code"
+  secret_name     = "DD_APP_KEY"
+  plaintext_value = "placeholder-added-manually"
   lifecycle {
     ignore_changes = [remote_updated_at]
   }
