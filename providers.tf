cbc6bbd04a959d2b4f9b424393c0e554eb639ccc
Max Onyx <devops@maxim.run>
2026-04-20 11:55:24 -0700

    test: sythentic test test

--- providers.tf
+++ providers.tf
@@ -6,6 +6,21 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 6.33.0, < 7.0.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = ">= 6.0, < 7.0.0"
+    }
+
+    datadog = {
+      source = "DataDog/datadog"
+    }
 
   }
 }
+
+provider "datadog" {
+  api_url = "https://us5.datadoghq.com/" # or DD_HOST
+  # set DD_API_KEY
+  # set DD_APP_KEY
+}
+
